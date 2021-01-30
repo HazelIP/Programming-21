141fbaa9498d6c49c7f9624cfc54594cdd346ed1
Hazel Ip <g00398581@gmit.ie>
2021-01-30 00:39:26 +0000

    week 2 extra question

---
+++ week2/nameAndAge.py
@@ -0,0 +1,16 @@
+# This program contains the extra question of week 2
+# Author: Ka Ling Ip
+
+#Q.17 This program reads in name and age and output a message
+name = input ("Enter your name:")
+age = input ("Enter your age:")
+print ("Hello {}, your age is {}.". format(name,age))
+
+#Q.18 This modified program has a tab at the end of the name
+print("Hello {}, \tyour age is {}.". format(name,age))
+
+#Q.19 This program outputs 21-4
+print(21-4)
+
+#Q.20 This program outputs if 2 equals 3
+print(2 == 3)
