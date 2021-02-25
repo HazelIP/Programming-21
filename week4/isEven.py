9228a8b6f7db4273e19f7e2c3a6c20ae0f371b23
Hazel Ka Ling Ip <hazelkalingip@fb.com>
2021-02-25 16:09:13 +0000

    upload from mac

---
+++ week4/isEven.py
@@ -0,0 +1,10 @@
+# This program tells if the number user entered is even or odd. 
+# Author: Ka Ling Ip
+
+# ask user to enter a number 
+num = int(input("Enter a number:"))
+# if a number is even, it can be divided by 2 with remainder = 0, otherwise is odd.
+if (num % 2) == 0:
+    print ("{} is an even number".format(num))
+else:
+    print ("{} is an odd number".format(num))
